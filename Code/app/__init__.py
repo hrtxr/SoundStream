44c9a47dd8e2fd9dee742e0ab8a8cc37f29dcb1f
saifeddine19 <saifeddinealjane@gmail.com>
2026-01-11 15:33:35 +0100

    j'ai changer la clé secrete mais c l'idée de qui

--- Code/app/__init__.py
+++ Code/app/__init__.py
@@ -3,6 +3,6 @@ import os
 
 app = Flask(__name__, static_url_path='/static')
 
-app.secret_key = 'Vive_Patrice_<3' # À CHANGER QUAND PASSAGE EN PROD
+app.secret_key = '5f352379324c22463451387a0aec5d2f' 
 
 from app.controllers import *
